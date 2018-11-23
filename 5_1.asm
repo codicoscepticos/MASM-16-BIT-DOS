c6073a3fc2c1f25e9c9f7dce885575d0d8688959
cogitator <bigdippergames@gmail.com>
2018-11-23 20:59:04 +0200

    5_1 updated

--- 5_1.asm
+++ 5_1.asm
@@ -15,10 +15,11 @@ ENDM
 
 ; Constants
 vidmem equ 0b800h
-cls_color equ 7
 scrw equ 80*25
-eom equ 0
+cls_color equ 7
+
 dollar equ '$'
+eom equ 0
 
 ; Segments
 stack segment use16 para stack
@@ -37,6 +38,7 @@ data segment use16
   line_2 dw 160d
   line_3 dw 320d
   line_24 dw 3680d
+  line_25 dw 3840d
 
   msg db 'Doste ta stoixeia tou 3*3 pinaka:'
   db eom
@@ -64,14 +66,13 @@ start:
     mov ax, vidmem
     mov es, ax
 
-    begin:
-    call cls
-    mov di, 0
+    read_nums:
+    mov di, 0 ; counter of elements/numbers; mainly, the destination index of the array where numbers are stored
 
-    again:
+    read_nums_loop:
+    call cls
+
     push di
-    call cls
-
     lea si, msg
     mov di, line_24
     print_prompt:
@@ -79,59 +80,65 @@ start:
       inc di
       cmp byte ptr[si], eom
     jne print_prompt
-
     pop di
-    mov cx, 10
-    mov bx, 0
 
-    readn1:
-      mov ah, 1 ; read the keyboard
-      int 21h
+    mov cx, 10  ; multiplication factor; later, by multiplying ax each time, the whole number
+                ; (temporarily referred by ax) is factored from units to thousands
+    mov bx, 0   ; actual referrer of whole number; via addition with ax it forms the whole number (in hex format)
 
-      mov ah, 0
+    read_num:
+      mov ah, 1 ; read the keyboard, stores string input (a single char) to al, echoes/displays character on screen
+      int 21h   ; (it doesn't wait for pressing ENTER, thus when you press a key, it immediately takes that as input and continues)
+      mov ah, 0 ; ah is set to 0 (from 1), because later ax is used to construct (via addition with bx) the typed number
+
       cmp al, '0'
-      jb readn2
+      jb store_read_num
       cmp al, '9'
-      ja readn2
+      ja store_read_num
+      ; if ENTER is pressed, which is equal to 0Dh, it will jump, too
 
-      sub al, '0' ; convert number (of type string) to hex format (e.g.: ('1'=31h) - ('0'=30h) = 1h)
+      sub al, '0' ; convert ASCII number (of type string) to hex format (e.g.: ('1'=31h) - ('0'=30h) = 1h)
 
       push ax
-      mov ax, bx
-      mul cx
-      mov bx, ax
-      pop ax
+      mov ax, bx  ; refers bx to ax, to
+      mul cx      ; multiply ax with cx, and store result to ax
+      mov bx, ax  ; ax is referred to bx which is the actual number holder
+                  ; (here, ax is used as a temp register,
+      pop ax      ; while its actual value is retained in the stack)
 
-      mov ah, 0
-      add bx, ax
-    jmp readn1
+      add bx, ax  ; the previously typed numbers, referred by bx, and constructed so they form a whole number,
+                  ; where each typed number takes the right place in the whole via the above multiplication (mul cx),
+                  ; are added with ax, which currently refers to the last typed number (as the current units)
+    jmp read_num
 
-    readn2:
-    mov ax, bx
-    mov array[di], ax
-    add di, 2
-    cmp di, 18
-    je break
-    jmp again
+    store_read_num:
+    mov array[di], bx
+    add di, 2   ; Each data location is 8 bit (i.e. 1 byte each), ax is 16 bit (2 bytes),
+                ; thus it takes 2 locations to be saved, and that's why di is increased by 2.
+    cmp di, 18  ; And that's why it checks for 9*2=18 (here it has nothing to do about color).
+    je disp_nums
+    jmp read_nums_loop
 
-    break:
+    disp_nums:
     call cls
-    mov bx, 1
-    mov si, 0
 
-    back:
+    mov bx, 0 ; counter of how many numbers have been displayed in the current row
+    mov si, 0 ; source index of which element/number to retrieve from array
+
+    disp_nums_loop:
       mov ax, array[si]
       mov dx, 0
       mov cx, 1000d
       div cx ; dx-ax / cx => dx = remainder, ax = quotient
-      push ax
 
       ; ----
       push ax
+      push ax ; for some reason 'hex2ascii' expects ax to be pushed twice
+              ; (or to find the input variable from a specific location of the stack)...
       call hex2ascii
       pop ax
 
-      disp buffer[3]
+      disp buffer[3]  ; display the thousands
 
       xchg dx, ax
       ; ----
@@ -141,14 +148,14 @@ start:
 
       mov dx, 0
       mov dl, al
-      push ax
 
       ; ----
+      push ax
       push ax
       call hex2ascii
       pop ax
 
-      disp buffer[3]
+      disp buffer[3]  ; display the hundreds
 
       xchg al, ah
       ; ----
@@ -159,38 +166,40 @@ start:
 
       mov dx, 0
       mov dl, al
-      push ax
 
       ; ----
+      push ax
       push dx
       call hex2ascii
       pop ax
 
-      disp buffer[3]
+      disp buffer[3]  ; display the tens
 
       xchg al, ah
       ; ----
 
       mov ah, 0
-      push ax
 
+      ; ----
+      push ax
       push ax
       call hex2ascii
       pop ax
 
-      disp buffer[3]
+      disp buffer[3]  ; display the units
       ; ---- ----
 
       disp space
-      add si, 2
+      add si, 2 ; increase si by 2 to get the next whole number in the next iteration
 
       inc bx
-      cmp bx, 4d
-    jne back
-      mov bx, 1
+      cmp bx, 3d  ; 3 is the limit of how many whole numbers should be displayed in the current row,
+                  ; once bx reach this, the code continues below
+    jne disp_nums_loop
+      mov bx, 0
       disp new_line
-      cmp si, 18d
-    jne back
+      cmp si, 18d ; same situation with 'cmp di, 18' above
+    jne disp_nums_loop
 
     ; MENU
     ; Print 0th message; which tells how to exit.
@@ -219,7 +228,7 @@ start:
       cmp al, '0'
       je l_exit
       cmp al, '1'
-      je begin
+      je read_nums
     jmp kbd
 
     l_exit:
@@ -229,17 +238,19 @@ start:
   ; Subroutines
   cls proc near
     push cx
-    mov di, 0
+    push di
+		mov di, 0
 
-    mov al, ' '
-    mov ah, cls_color
+		mov al, ' '
+		mov ah, cls_color
 
-    mov cx, scrw
-    rep stosw ; es:di <- ax, di <- di+2
+		mov cx, scrw
+		rep stosw ; es:di <- ax, di <- di+2
 
-    pop cx
-    ret
-  cls endp
+    pop di
+		pop cx
+		ret
+	cls endp
 
   HEX2ASCII PROC NEAR
     ; Converts a word variable to ASCII
