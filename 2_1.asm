66cb03c4018f826d4bebadaf346e0fc372de99d3
cogitator <cogitator@users.noreply.github.com>
2019-03-01 15:39:05 +0200

    final version

--- 2_1.asm
+++ 2_1.asm
@@ -1,17 +1,16 @@
 .386
+ASSUME CS:code
 
 STACK_SEG segment para STACK
-    db 256 dup (0)
+  db 256 dup (0)
 STACK_SEG ends
 
 code SEGMENT 'CODE'
-ASSUME CS:code
-
 start:
-    MOV DL, 5ah
-    MOV AH, 02h
-    INT 21h
-    MOV AH, 4Ch
-    INT 21h
-    code ENDS
+  MOV DL, 5ah
+  MOV AH, 02h
+  INT 21h
+  MOV AH, 4Ch
+  INT 21h
+code ENDS
 END start
