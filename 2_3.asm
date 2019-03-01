9222ad77b035c3fd77c16ecdbc7b735d8ddbccfe
cogitator <cogitator@users.noreply.github.com>
2019-03-01 15:41:29 +0200

    final version

--- 2_3.asm
+++ 2_3.asm
@@ -1,30 +1,28 @@
 .386
-
-mycode SEGMENT USE16 PARA PUBLIC 'CODE'
 ASSUME CS:mycode, DS:mydata
 
+stack SEGMENT STACK
+  DW 64 dup (0)
+stack ends
+
+mydata SEGMENT para PUBLIC 'DATA'
+  NUM1 DW 0Fh
+  NUM2 DW 05h
+  ANS DW ?
+mydata ends
+
+mycode SEGMENT USE16 PARA PUBLIC 'CODE'
 start:
-    MOV AX, mydata
-    MOV DS, AX
-    SUB DX, DX
+  MOV AX, mydata
+  MOV DS, AX
+  SUB DX, DX
 
-    MOV AX, NUM1
-    MOV BX, NUM2
-    SUB AX, BX
-    MOV ANS, AX
+  MOV AX, NUM1
+  MOV BX, NUM2
+  SUB AX, BX
+  MOV ANS, AX
 
-    MOV AH, 4Ch
-    INT 21h
-    mycode ENDS
-
-    mydata SEGMENT para PUBLIC 'DATA'
-        NUM1 DW 0Fh
-        NUM2 DW 05h
-        ANS DW ?
-    mydata ends
-
-    stack SEGMENT STACK
-        DW 64 dup (0)
-    stack ends
-
+  MOV AH, 4Ch
+  INT 21h
+mycode ENDS
 END start
