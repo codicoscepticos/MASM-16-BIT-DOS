25444411b0387300b18357e9e4b2cf19435cf452
cogitator <cogitator@users.noreply.github.com>
2019-03-01 15:40:23 +0200

    final version

--- 2_2.asm
+++ 2_2.asm
@@ -1,25 +1,24 @@
 .386
-
-code SEGMENT USE16 PARA PUBLIC 'CODE'
 ASSUME CS:code, DS:data, SS:stack
 
+stack SEGMENT STACK
+  DW 64 dup (0)
+stack ends
+
+data SEGMENT para PUBLIC
+  string DB "FirstName LastName", 13, 10, "$"
+data ends
+
+code SEGMENT USE16 PARA PUBLIC 'CODE'
 start:
-    MOV AX, data
-    MOV DS, AX
-    MOV DX, OFFSET string
+  MOV AX, data
+  MOV DS, AX
 
-    MOV AH, 09h
-    INT 21h
-    MOV AH, 4Ch
-    INT 21h
-    code ENDS
-
-    data SEGMENT para PUBLIC
-        string DB "Name Surname", 13, 10, "$"
-    data ends
-
-    stack SEGMENT STACK
-        DW 64 dup (0)
-    stack ends
-
+  MOV DX, OFFSET string
+  MOV AH, 09h
+  INT 21h
+  
+  MOV AH, 4Ch
+  INT 21h
+code ENDS
 END start
